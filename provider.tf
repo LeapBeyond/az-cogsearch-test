cb3b9bf91b50b0fc43ce6d078de350f461ba2590
Robert Hook <rahook@gmail.com>
2020-11-16 10:54:09 +0000

    begin breaking project into separate examples

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider azurerm {
-  version                 = "~>2.0"
-  client_certificate_path = var.client_certificate_path
-  client_id               = var.client_id
-  subscription_id         = var.subscription_id
-  tenant_id               = var.tenant_id
-  features {}
-}
